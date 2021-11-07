6d19c90589aea3cbd538a11ef45bbb41f34a9fd3
Akshaya Biswal <akshaya.join@gmail.com>
2021-11-07 20:57:23 +0530

    Update User Controller Added

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,29 @@
-class User {
-  
+const User = require("../models/User");
+
+class UserController {
+  async updated(req, res) {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString();
+    }
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        {
+          new: true,
+        }
+      );
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
 }
+
+module.exports = new UserController();
