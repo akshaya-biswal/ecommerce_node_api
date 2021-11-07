9e34228baa43ff2e2c0744f7ef7bce6e8605dd22
Akshaya Biswal <akshaya.join@gmail.com>
2021-11-07 09:35:59 +0530

    Register router added

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+const CryptoJS = require("crypto-js");
+
+class AuthController {
+  async register(req, res) {
+    const { username, email, password } = req.body;
+
+    const newUser = new User({
+      username,
+      email,
+      password: CryptoJS.AES.encrypt(password, process.env.PASS_SEC).toString(),
+    });
+
+    try {
+      const saveUser = await newUser.save();
+      res.status(201).json(saveUser);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+}
+
+module.exports = new AuthController();
--- router.js
+++ router.js
@@ -1,1 +1,4 @@
 const router = require("express").Router();
+const authController = require("./controllers/auth");
+
+router.post("/api/register", authController.register);
